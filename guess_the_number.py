d88c79ce54f0a495960f03e5f6cb3fdcff121e38
DeeStAcE <dawidfirmowski@gmail.com>
2023-03-24 11:54:16 +0100

    done

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,29 +1,29 @@
-def main():
+def guess_the_number():
     print('Think od a number between 0 and 1000. I will guess it in 10 steps max. ->')
     min_ = 0
     max_ = 1000
+    user_ans = ''
 
-    guess = int((max_ - min_) / 2) + min_
-    print(f'I am guessing: {guess}')
-
-    user_ans = input('Did i guess it?\n').lower()
     while user_ans != 'yes':
+        guess = int((max_ - min_) / 2) + min_
+        print(f'I am guessing: {guess}')
+        user_ans = get_an_ans()
         if user_ans == 'too small':
             min_ = guess
-            guess = int((max_ - min_) / 2) + min_
-            print(f'I am guessing: {guess}')
-            user_ans = input('Did i guess it?\n').lower()
         elif user_ans == 'too big':
             max_ = guess
-            guess = int((max_ - min_) / 2) + min_
-            print(f'I am guessing: {guess}')
-            user_ans = input('Did i guess it?\n').lower()
-        else:
-            print('You are cheating!')
-            print(f'I am guessing: {guess}')
-            user_ans = input('Did i guess it?\n').lower()
     print('I won!')
 
 
+def get_an_ans():
+    correct_ans = ('too small', 'too big', 'yes')
+    while True:
+        user_ans = input('Did i guess it?\n').lower()
+        if user_ans in correct_ans:
+            break
+        print('Enter a valid answer (too small, too big or yes)')
+    return user_ans
+
+
 if __name__ == '__main__':
-    main()
+    guess_the_number()
